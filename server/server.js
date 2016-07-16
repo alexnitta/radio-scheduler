c3c79a9b3c2d70260c79688ad9bfa8f4e865d24c
Alex Nitta <a.j.nitta@gmail.com>
2016-07-16 14:45:42 -0700

    Clean up file structure

--- server/server.js
+++ server/server.js
@@ -1,48 +1,48 @@
+// import express modules
 const express = require('express');
-const path = require('path');
+const fs = require('fs');
+const http = require('http');
 const https = require('https');
+const path = require('path');
 const router = express.Router();
+
+// import system settings
 const networkSettings = '/../config/network-settings';
 const PORT = networkSettings.PORT;
+const ENV = process.env.NODE_ENV;
+const sslSettings = {
+  key: fs.readFileSync('/config/keys/privKey.pem'),
+  cert: fs.readFileSync('/config/keys/cert.pem')
+};
 
-pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
-  const app = express();
- 
-  app.get('/',  requireAuth, function(req, res){
-    res.send('o hai!');
-  });
-  
-  // send requests for root to index.html
-  app.get('/', requireAuth, function(req, res) {
-    res.sendFile(path.resolve('./client/index.html'));
-  });
- 
-  https.createServer({key: keys.serviceKey, cert: keys.certificate}, app).listen(PORT);
-});
-
-
-
-// instantiate server
+// instantiate server application
 const app = express();
 
 // serve static files from client folder only
 app.use('/static', express.static(__dirname + '/../client'));
 
+// send requests for root to index.html
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('./client/index.html'));
+});
 
-
-// redirect wildcard requests to root
+// redirect wild card requests to root
 app.get('/*', function (req, res) {
   res.redirect('/');
 });
 
-// Run server listening on the local environment
-const server = https.createServer(app);
+// create an HTTPS service 
+https.createServer(sslSettings, app).listen(PORT, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('radio-scheduler server is listening on port: ' + PORT + ' in ' + ENV + ' environment');
+});
 
-server.listen(PORT);
-console.log('radio-scheduler server listening on port ' + PORT + ' in ' + process.env.NODE_ENV + ' mode');
-
-// socket.io
-const io = require('socket.io')(server);
-require('./helpers/socket')(io);
+// redirect from http port 80 to https
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
 
 module.exports = app;
