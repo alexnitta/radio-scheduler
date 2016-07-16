3bb769743d0632077df952c70f54d190d773cb4c
Alex Nitta <a.j.nitta@gmail.com>
2016-07-16 07:39:28 -0700

    Install more npm packages

--- config/network-settings.js
+++ config/network-settings.js
@@ -1,6 +1,6 @@
 const networkSettings = {
-  PORT: process.env.PORT || 443,
-  URL: process.env.NODE_ENV === 'production' ? 'https://production.url.REPLACE_ME' : 'https://127.0.0.1',
+  PORT: process.env.PORT || 4300,
+  URL: process.env.NODE_ENV === 'production' ? 'https://production.url.REPLACE_ME:443' : 'https://127.0.0.1:4300',
   HOST: process.env.NODE_ENV === 'production' ? 'production.url.REPLACE_ME' : '127.0.0.1'
 };
 
