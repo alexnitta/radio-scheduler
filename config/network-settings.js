e5c8108bcc274e29449663ad954b52ec35a31be0
Alex Nitta <a.j.nitta@gmail.com>
2016-07-16 07:51:14 -0700

    Switch logic on setting network location from ENV

--- config/network-settings.js
+++ config/network-settings.js
@@ -1,7 +1,7 @@
 const networkSettings = {
   PORT: process.env.PORT || 4300,
-  URL: process.env.NODE_ENV === 'production' ? 'https://production.url.REPLACE_ME:443' : 'https://127.0.0.1:4300',
-  HOST: process.env.NODE_ENV === 'production' ? 'production.url.REPLACE_ME' : '127.0.0.1'
+  URL: process.env.NODE_ENV === 'development' ? 'https://127.0.0.1:4300' : 'https://production.url.REPLACE_ME:443',
+  HOST: process.env.NODE_ENV === 'development' ? '127.0.0.1' : 'production.url.REPLACE_ME'
 };
 
 module.exports = networkSettings;
