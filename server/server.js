06a3c79e249893e9d8d94d1fde223efe411140e1
Alex Nitta <a.j.nitta@gmail.com>
2016-07-17 06:23:32 -0700

    Switch to self-signed SSL for development

--- server/server.js
+++ server/server.js
@@ -11,12 +11,30 @@ const networkSettings = require ('./config/network-settings');
 const PORT = networkSettings.PORT;
 const HOST = networkSettings.HOST;
 const ADMIN_EMAIL = networkSettings.ADMIN_EMAIL;
+const ENV = process.env.NODE_ENV;
+
 var sslConfig = {
   key: fs.readFileSync(__dirname + '/config/private/ssl-keys/privKey.pem'),
   cert: fs.readFileSync(__dirname + '/config/private/ssl-keys/cert.pem')
 };
 
+// SERVER SETUP V1: self signed SSL certificates
+
+// instantiate server application
 const app = express();
+
+// serve static files from client folder only
+app.use('/static', express.static(__dirname + '/../client'));
+
+// send requests for root to index.html
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('./client/index.html'));
+});
+
+// redirect wild card requests to root
+app.get('/*', function (req, res) {
+  res.redirect('/');
+});
 
 // Create an HTTPS service 
 https.createServer(sslConfig, app).listen(PORT, function(err) {
@@ -35,13 +53,11 @@ http.createServer(function (req, res) {
 
 
 /*
+SERVER SETUP V2: automated SSL certificates from letsencrypt-express module
 
-const ENV = process.env.NODE_ENV;
+
 const leConfDir = '/config/private/ssl-keys';
 // const leConfDir = require('os').homedir() + '/letsencrypt/etc';
-
-
-
 
 // set up which certificate server to use, depending on ENV
 const LEX = ENV === 'development' ? require('letsencrypt-express').testing() : require('letsencrypt-express');
@@ -71,6 +87,7 @@ const redirectHttp = function() {
 
 
 const serveHttps = function() {
+
   // instantiate server application
   const app = express();
   
