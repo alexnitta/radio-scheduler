a5c947e9f3536981692bce40a1bc3b911a82b18b
Alex Nitta <a.j.nitta@gmail.com>
2016-07-17 15:58:14 -0700

    Set up basic location services

---
+++ client/dist/Location.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var userLocation = {
+  lat: 0,
+  long: 0
+};
+
+if ('geolocation' in navigator) {
+  /* geolocation is available */
+  navigator.geolocation.getCurrentPosition(function (position) {
+    userLocation.lat = position.coords.latitude;
+    userLocation.long = position.coords.longitude;
+    console.log('userLocation is: ', userLocation);
+  });
+} else {
+  /* geolocation IS NOT available */
+  console.log('userLocation is not available');
+}
